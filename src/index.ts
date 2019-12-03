66395071c112705f2c62b7cd6b291e5d2d29ee39
Felds Liscia <dev@felds.com.br>
2019-12-03 15:16:40 -0300

    add build script

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
 import AsyncPlayer from "./AsyncPlayer";
 
-declare const root: HTMLDivElement;
-declare const play: HTMLButtonElement;
-
-const wait = (t: number) => (x: any) =>
-  new Promise(win => setTimeout(() => win(x), t));
-
-const pl = new AsyncPlayer(root, {
-  videoId: "H6u0VBqNBQ8"
-});
-
-play.onclick = () => {
-  pl.play()
-    .then(wait(3000))
-    .then(() => pl.pause());
-};
+export default AsyncPlayer;
