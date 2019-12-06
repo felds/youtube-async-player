8b2fed0290cc142fb5a7110db5b3d9e121c78735
Felds Liscia <dev@felds.com.br>
2019-12-06 18:34:23 -0300

    add tests

--- src/EventEmitter.test.ts
+++ src/EventEmitter.test.ts
@@ -1,5 +1,39 @@
 import EventEmitter from "./EventEmitter";
 
-describe("on", () => {
-  test("it registers a new event", () => {});
+test("it registers and emits a new event", () => {
+  const emitter = new EventEmitter();
+  const stub = jest.fn();
+
+  emitter.on("doStuff", stub);
+  emitter.emit("doStuff");
+  emitter.emit("doStuff");
+  emitter.emit("doStuff");
+
+  expect(stub).toBeCalledTimes(3);
+});
+
+test("it registers an event and runs it only once", () => {
+  const emitter = new EventEmitter();
+  const stub = jest.fn();
+
+  emitter.once("doStuff", stub);
+  emitter.emit("doStuff");
+  emitter.emit("doStuff");
+  emitter.emit("doStuff");
+
+  expect(stub).toBeCalledTimes(1);
+});
+
+test("it unregisters an event", () => {
+  const emitter = new EventEmitter();
+  const stub = jest.fn();
+
+  emitter.on("doStuff", stub);
+  emitter.emit("doStuff", stub);
+  emitter.emit("doStuff", stub);
+  emitter.off("doStuff", stub);
+  emitter.emit("doStuff", stub);
+  emitter.emit("doStuff", stub);
+
+  expect(stub).toBeCalledTimes(2);
 });
